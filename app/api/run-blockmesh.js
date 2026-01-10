655f9f8a22c1de1a3575137964af252acb0d7abf
chinmay-s-patil <patil.chinmay3031@gmail.comclear>
2026-01-10 13:38:22 +0100

    Update 0.63

--- app/api/run-blockmesh.js
+++ app/api/run-blockmesh.js
@@ -1,4 +1,7 @@
 // pages/api/run-blockmesh.js
+
+export { default } from '../../app/api/run-blockmesh.js';
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
